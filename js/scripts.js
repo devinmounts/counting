013274db4632437535551f652b6c41fe07d44a99
Devin Mounts and Eric Shull <devin@>
2018-06-05 10:20:10 -0700

    add for statement and .pop to return the last indexed array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 $(function(){
-  var initial = 0
-  var goal = parseInt($("#countTo").val());
-  var final = parseInt($("#countBy").val());
-  var output = [];
+
 
   $("#submit").click(function(e){
     e.preventDefault()
-    
-    console.log(goal);
-    for (i=0; i<=goal.length; i+=final) {
+    var initial = 0
+    var goal = parseInt($("#countTo").val());
+    var final = parseInt($("#countBy").val());
+    var arrays = [];
+    var output = [];
 
-      if (final <= goal){
-
-      }else {
-
-      }
-
+    for (var index=0; index<=goal; index+=final) {
+      output.push(index);
+      arrays.push(output);
     }
 
-
+    // console.log(output);
+      console.log(arrays);
+      var finalArray= arrays.pop();
+      console.log(finalArray);
+      $("#output").prepend("<li>"+ finalArray + "</li>");
   });
+
+
 });
